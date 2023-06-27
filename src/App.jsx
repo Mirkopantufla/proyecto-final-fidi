8b45d3f524a6789da20c610a3d1e4674fe04c2ea
Paz Valenzuela <pazvalenzu@gmail.com>
2023-06-27 12:17:15 -0700

    Pazcommit27/06

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,86 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Formulario from './componentes/Formulario';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logoImage from "./logo1.png";
 import logoImage2 from "./logo2rosa.png";
 
+function Home() {
+  return <h2>Home</h2>;
+}
 
 function App() {
   return (
-    <div>
-      <nav class="navbar navbar-light">
-        <a class="navbar-brand" href="#">
-          <img
-            src={logoImage}
-            alt="Logo"
-            width="150"
-            height="150"
-            className="logo"
-          />
-        </a>
-      </nav>
+    <Router>
+      <div>
+        <nav className="navbar navbar-light">
+          <a className="navbar-brand" href="/">
+            <img
+              src={logoImage}
+              alt="Logo"
+              width="150"
+              height="150"
+              className="logo"
+            />
+          </a>
+        </nav>
 
-      {/* Contenedor principal */}
-      <div className="container main-container custom-bg rounded">
-        <div className="row">
-          {/* Sección de explorar */}
-          <div className="col-sm-6">
-            <div className="explore-container  p-4">
-              <h2>Explora personas que quieran intercambiar conocimiento</h2>
-              <button className="btn btn-dark">Explorar</button>
+        <div className="container main-container custom-bg rounded">
+          <div className="row">
+            <div className="col-sm-6">
+              <div className="explore-container  p-4">
+                <h2>Explora personas que quieran intercambiar conocimiento</h2>
+                <button className="btn btn-dark">Explorar</button>
+              </div>
             </div>
-          </div>
-
-          {/* Sección de registro */}
-          <div className="col-sm-6">
-            <div className="registration-container  p-4">
-              <h1>Regístrate en Fidi</h1>
-              <form>
-                <div className="form-group">
-                  <label htmlFor="name">Nombre:</label>
-                  <input type="text" className="form-control" id="name" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="email">Correo electrónico:</label>
-                  <input type="email" className="form-control" id="email" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="password">Contraseña:</label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                  />
-                </div>
-                <br />
-                <button type="submit" className="btn btn-dark">
-                  Registrarse
-                </button>
-              </form>
+            <div className="col-sm-6">
+              <div className="registration-container  p-4">
+                <h1>Regístrate en Fidi</h1>
+                <form>
+                  <div className="form-group">
+                    <label htmlFor="name">Nombre:</label>
+                    <input type="text" className="form-control" id="name" />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="email">Correo electrónico:</label>
+                    <input type="email" className="form-control" id="email" />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="password">Contraseña:</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="password"
+                    />
+                  </div>
+                  <br />
+                  <Link to="/formulario" className="btn btn-dark">
+                    Registrarse
+                  </Link>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* Pie de página */}
-      <footer className="text-center py-4">
-     
-      <p>&copy; 2023 Fidi. Todos los derechos reservados.</p>
-      <div className="container position-relative">
-        <div className="row">
-          <div className="col-sm-12 text-center">
-            <img src={logoImage2} alt="Logo" width="50"height="40"className="footer-logo" />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/formulario" element={<Formulario />} />
+        </Routes>
+
+        <footer className="text-center py-4">
+          <p>&copy; 2023 Fidi. Todos los derechos reservados.</p>
+          <div className="container position-relative">
+            <div className="row">
+              <div className="col-sm-12 text-center">
+                <img src={logoImage2} alt="Logo" width="50" height="40" className="footer-logo" />
+              </div>
+            </div>
           </div>
-        </div>
+        </footer>
       </div>
-    </footer>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/componentes/Formulario.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Formulario.css'; // Importa tu archivo de estilos CSS personalizado para el formulario
+
+function Formulario() {
+  const [intereses, setIntereses] = useState('');
+  const [aprendizaje, setAprendizaje] = useState('');
+  const [biografia, setBiografia] = useState('');
+  const [fotoPerfil, setFotoPerfil] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes realizar acciones adicionales, como enviar los datos a un servidor, guardarlos en una base de datos, etc.
+    console.log('Intereses:', intereses);
+    console.log('Aprendizaje:', aprendizaje);
+    console.log('Biografía:', biografia);
+    console.log('Foto de perfil:', fotoPerfil);
+    // Luego puedes redirigir al usuario a otra página o mostrar un mensaje de éxito, según tus necesidades
+  };
+
+  return (
+    <div className="container-fluid bg-dark">
+      <div className="row">
+        <div className="col-sm-6 mx-auto">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="intereses" className="text-light">Intereses:</label>
+              <input
+                type="text"
+                className="form-control"
+                id="intereses"
+                value={intereses}
+                onChange={(e) => setIntereses(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="aprendizaje" className="text-light">Lo que quiero aprender:</label>
+              <input
+                type="text"
+                className="form-control"
+                id="aprendizaje"
+                value={aprendizaje}
+                onChange={(e) => setAprendizaje(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="biografia" className="text-light">Biografía:</label>
+              <textarea
+                className="form-control"
+                id="biografia"
+                value={biografia}
+                onChange={(e) => setBiografia(e.target.value)}
+                maxLength={500}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="fotoPerfil" className="text-light">Foto de perfil:</label>
+              <input
+                type="file"
+                className="form-control-file"
+                id="fotoPerfil"
+                onChange={(e) => setFotoPerfil(e.target.files[0])}
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">Enviar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Formulario;

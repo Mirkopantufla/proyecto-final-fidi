5a3a776eddfdfd1c3fd5142122debe45c1a0b209
Mirko Pasten <mikish_ash@hotmail.com>
2023-06-28 02:05:28 -0400

    Añadido importe de bootstrap en main JSX, Cambiado componente Chat de carpeta componentes a pages. Componente para enlistar Usuarios en el chat junto a un css

---
+++ src/componentes/ListaUsuariosChat.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { IoReturnUpBackSharp } from 'react-icons/io5'
+
+const ListaUsuariosChat = ({ usuario, estado, ultimoMsjRecibido, srcFotografia }) => {
+    return (
+        <div className='d-flex border-top border-dark mb-2'>
+            <img className='me-4 rounded-circle p-2' src={srcFotografia} alt="perro" />
+            <div className='d-flex flex-column justify-content-center align-items-center'>
+                <h3 className=''>{usuario}</h3>
+                <p className=''>{estado == true ? "Online" : "Offline"}</p>
+                <div className='d-flex'>
+                    <IoReturnUpBackSharp className='me-2 fs-4' />
+                    <p>{ultimoMsjRecibido}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ListaUsuariosChat
---
+++ src/pages/Chat.jsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import "../estilos/Chat.css"
+import { AiFillCloseCircle } from 'react-icons/ai'
+import { MdInsertEmoticon } from 'react-icons/md'
+import { BsThreeDots } from 'react-icons/bs'
+import ListaUsuarios from '../componentes/ListaUsuariosChat'
+
+//Lista de prueba para mapear Usuarios en el chat
+//Deberan ser las personas que han hecho match las que se habilitaran para hablar
+//El chat se almacenara en la base de datos y debera ser traido desde allí
+const listaPruebaUsuarios = [
+    {
+        usuario: "Mirko Pasten",
+        estado: false,
+        ultimoMsjRecibido: "ola mundo",
+        srcFotografia: "https://picsum.photos/id/237/120/120"
+    },
+    {
+        usuario: "Rosa Deltransito",
+        estado: false,
+        ultimoMsjRecibido: "enseña bien oe",
+        srcFotografia: "https://picsum.photos/id/238/120/120"
+    },
+    {
+        usuario: "Armando Losas",
+        estado: false,
+        ultimoMsjRecibido: "paralelepipedo",
+        srcFotografia: "https://picsum.photos/id/239/120/120"
+    }
+];
+
+const Chat = () => {
+    return (
+        <div className='container-fluid'>
+            <div className="row">
+                <div className="col-3 text-center m-auto">
+                    <h2 className='ms-2'>Lista Contactos</h2>
+                </div>
+                <div className="col-9">
+                    <div className="row">
+                        <div className="col-lg-3 col-md-3 my-3">
+                            <img className='border rounded-5' src="https://picsum.photos/id/237/150/150" alt="" />
+                        </div>
+                        <div className="col-lg-5 col-md-7 my-auto d-flex flex-column justify-content-left">
+                            <h4>Nombre Contacto</h4>
+                            <div className=''>
+                                <button className='btn btn-dark'>interes</button>
+                                <button className='btn btn-dark'>interes</button>
+                                <button className='btn btn-dark'>interes</button>
+                                <button className='btn btn-dark'>interes</button>
+                            </div>
+
+                        </div>
+                        <div className="col-lg-4 col-md-2 mt-2 d-flex justify-content-end mt-5">
+                            <div className="dropdown">
+                                <button className="btn btn-dark" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <BsThreeDots />
+                                </button>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Añadir a Favoritos</a></li>
+                                    <li><a className="dropdown-item" href="#">Reportar</a></li>
+                                </ul>
+                            </div>
+                            <AiFillCloseCircle className='fs-1 text-dark ms-2 bg-light border border-dark rounded-circle' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* Ocupo clase custom 'chat' para mantener la  altura del cuadro de mensaje*/}
+            <div className="row chat px-3">
+                <div className="col-3 text-center">
+                    {
+                        //Mapeo de los objetos contenidos en el array listaPruebaUsuarios
+                        //Extraigo cada usuario y lo muestro en el chat, suponiendo que son los match 
+                        listaPruebaUsuarios.map((usuario, index) => {
+                            return (
+                                <ListaUsuarios
+                                    key={index}
+                                    usuario={usuario.usuario}
+                                    estado={usuario.estado}
+                                    ultimoMsjRecibido={usuario.ultimoMsjRecibido}
+                                    srcFotografia={usuario.srcFotografia}
+                                />
+                            );
+                        })
+                    }
+                </div>
+                <div className="col-9 text-center bg-light rounded-4" style={{ height: "72vh" }}>
+                    Hola prros
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-3 text-center">
+                    Algo
+                </div>
+                <div className="col-9 d-flex mt-lg-3" style={{ margin: "-15px" }}>
+                    <MdInsertEmoticon className='fs-1 me-1' />
+                    <input className='form-control' type="text" />
+                    <button className='btn btn-dark ms-3'>Archivo</button>
+                    <button className='btn btn-dark ms-3'>Audio</button>
+                    <button className='btn btn-dark ms-3'>Enviar</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Chat

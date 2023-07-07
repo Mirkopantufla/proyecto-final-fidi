fc0e2f64710304d531ae64e0a1e9d8e580ad744f
user1fernan <fernandamedinajara2@gmail.com>
2023-07-06 21:40:55 -0400

    botones y colores

--- src/pages/Dashboardv.jsx
+++ src/pages/Dashboardv.jsx
@@ -6,6 +6,11 @@ import MadCat3 from '../dashimg/madcat3.jpg';
 import MadCat4 from '../dashimg/madcat4.jpg';
 import MadCat5 from '../dashimg/madcat5.jpg';
 
+
+//font-family: 'Bagel Fat One', cursive;
+//font-family: 'Modak', cursive;
+//font-family: 'Shrikhand', cursive; 
+
 const Dashboardv = () => {
   return (
 
@@ -17,8 +22,25 @@ const Dashboardv = () => {
 
           <div className="d-flex justify-content-center align-items-center flex-column">
 
-            <div className="card" style={{ width: "18rem" }}>
-              <img src={MadCat5} className="card-img-top" alt="..." />
+            <div className="card" style={{ width: "35rem" , height: "45rem" , marginTop:"20px" }}>
+              <img src={MadCat5} className="card-img-top" style={{width: "35rem" , height: "35rem"}} alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Título de la noticia</h5>
+                <p className="card-text">Descripción de la noticia</p>
+                <a href="#" className="btn btn-primary">Ver más</a>
+              </div>
+            </div>
+
+            <div className="card" style={{ width: "35rem" , height: "45rem" , marginTop:"20px" }}>
+              <img src={MadCat5} className="card-img-top" style={{width: "35rem" , height: "35rem"}} alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Título de la noticia</h5>
+                <p className="card-text">Descripción de la noticia</p>
+                <a href="#" className="btn btn-primary">Ver más</a>
+              </div>
+            </div>
+            <div className="card" style={{ width: "35rem" , height: "45rem" , marginTop:"20px" }}>
+              <img src={MadCat5} className="card-img-top" style={{width: "35rem" , height: "35rem"}} alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Título de la noticia</h5>
                 <p className="card-text">Descripción de la noticia</p>
@@ -42,46 +64,58 @@ const Dashboardv = () => {
               <h5>Nombre de usuario</h5>
               <p>Frase linda</p>
               <div className="button-container">
-                <button className="btn btn-light">
+                <button className="btn btn-light-subtle" style={{backgroundColor: "#eaeaf0c9"}}>
                   <FaEnvelopeOpen/>
                   </button>
-                <button className="btn btn-light">
+                <button className="btn btn-light-subtle"  style={{backgroundColor: "#eaeaf0c9"}}>
                   <PiPencilSimpleLineBold/>
                 </button>
               </div>
             </div>
 
-            <div className="user-list">
+            <div className="user-list" style={{marginTop: '20px'}}>
 
               <div className="user-item border-bottom border-secondary-subtle">
                 <img src={MadCat4} alt="Foto de perfil" className="rounded-circle" />
                 <div className="user-details">
-                  <p style={{marginBottom: '-5px' , fontWeight: '500'}}>Alejandro Miranda</p>
-                  <p style={{fontSize:'15px'}}>Calla burro</p>
+                  <p style={{marginBottom: '-5px' , fontWeight: '600'}}>Alejandro Miranda</p>
+                  <p style={{fontSize:'15px' , marginTop:'2px'}}>Calla burro</p>
+                  <button className="btn-user-detail btn-sm" style={{fontWeight:"bold"}}>
+                  +Match
+                  </button>
                 </div>
               </div>
 
               <div className="user-item border-bottom border-secondary-subtle">
                 <img src={MadCat4} alt="Foto de perfil" className="rounded-circle" />
                 <div className="user-details">
-                  <p style={{marginBottom: '-5px' , fontWeight: '500'}}>Armando Losas</p>
-                  <p style={{fontSize:'15px'}}>Q pedo wey</p>
+                  <p style={{marginBottom: '-5px' , fontWeight: '600'}}>Armando Losas</p>
+                  <p style={{fontSize:'15px' , marginTop:'2px'}}>Q pedo wey</p>
+                  <button className="btn-user-detail btn-sm" style={{fontWeight:"bold"}}>
+                  +Match
+                  </button>
                 </div>
               </div>
 
               <div className="user-item border-bottom border-secondary-subtle">
                 <img src={MadCat4} alt="Foto de perfil" className="rounded-circle" />
                 <div className="user-details">
-                  <p style={{marginBottom: '-5px' , fontWeight: '500'}}>Rosa Deltransito</p>
-                  <p style={{fontSize:'15px'}}>Nada manito</p>
+                  <p style={{marginBottom: '-5px' , fontWeight: '600'}}>Rosa Deltransito</p>
+                  <p style={{fontSize:'15px', marginTop:'2px'}}>Nada manito</p>
+                  <button className="btn-user-detail btn-sm" style={{fontWeight:"bold"}}>
+                  +Match
+                  </button>
                 </div>
               </div>
 
               <div className="user-item border-bottom border-secondary-subtle">
                 <img src={MadCat4} alt="Foto de perfil" className="rounded-circle" />
                 <div className="user-details">
-                  <p style={{marginBottom: '-5px' , fontWeight: '500'}}>Matias Pasten</p>
-                  <p style={{fontSize:'15px'}}>Ya merito llegamos?</p>
+                  <p style={{marginBottom: '-5px' , fontWeight: '600'}}>Matias Pasten</p>
+                  <p style={{fontSize:'15px' , marginTop:'2px'}}>Ya merito llegamos?</p>
+                  <button className="btn-user-detail btn-sm" style={{fontWeight:"bold"}}>
+                  +Match
+                  </button>
                 </div>
               </div>
 
